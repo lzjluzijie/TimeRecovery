5dd2d362571696a42b225095e88b35bc21d0699c
halulu <lzjluzijie@gmail.com>
2018-12-11 16:21:25 +0800

    Update TimeRecovery.sh

--- TimeRecovery.sh
+++ TimeRecovery.sh
@@ -1,13 +1,27 @@
-for file in `ls files`
+cd files
+if [ ! -d notime ]; then
+  mkdir notime
+fi
+
+for file in `ls`
 do
+  if [ -d "$file" ]; then
+    continue
+  fi
+  
   time=`echo $file | sed -nr 's/[^0-9]//gp'`
+  
   if [ ${#time} == "17" ]; then 
 	time=${time:0:14}
   fi
+  
   if [ ${#time} != "14" ]; then 
+    echo cannot find time: $file
+    mv $file notime/
 	continue
   fi
+  
   time=${time:0:12}.${time:12:14}
   echo $time $file
-  touch -t $time files/$file
+  touch -t $time $file
 done
